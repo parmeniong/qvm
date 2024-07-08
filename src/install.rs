8bcf32093a141e0c922508995e9392f8cca8f879
parmeniong <par.graikos@icloud.com>
2024-07-09 00:52:15 +0300

    Initial commit

---
+++ src/install.rs
@@ -0,0 +1,5 @@
+use semver::Version;
+
+pub fn install(version: Version) {
+    println!("Installing version {}...", version);
+}
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+mod install;
+
+use install::install;
+use semver::Version;
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand)]
+enum Command {
+    /// Install a version of Quasar
+    Install {
+        /// The version of Quasar to install
+        #[arg(short, long)]
+        version: String
+    }
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Command::Install { version } => {
+            let version = Version::parse(&version).unwrap();
+            install(version);
+        }
+    }
+}
